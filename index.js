cc9bbcf5d0e9da7d0f0515d7cedd086b50814e66
Germán Ezequiel Horianski <103540419+GermanEH@users.noreply.github.com>
2023-08-30 12:18:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ export const config = {
     process.env.DATABASE_URI || process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/",
   // cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  //masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   //serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions

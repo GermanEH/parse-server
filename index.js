6ca9a269f3af6c789cdffe7b08c01896c67bb6d8
Germán Ezequiel Horianski <103540419+GermanEH@users.noreply.github.com>
2023-08-30 18:10:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ export const config = {
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKeyIps: ['0.0.0.0/0', '::/0'],
   serverURL: 'https://parse-server-production-5bcc.up.railway.app/parse' || process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
-  masterKeyIps: ['::ffff:192.168.0.4']
 };
 console.log(config.appId, config.masterKey, config.serverURL)
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server

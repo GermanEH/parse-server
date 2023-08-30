b898713106149d1bfa7f5a9610573cc5bbc384f8
Germán Ezequiel Horianski <103540419+GermanEH@users.noreply.github.com>
2023-08-30 15:48:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ export const config = {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
 };
-console.log('variables:', config.databaseURI, config.cloud, config.appId, config.masterKey)
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
